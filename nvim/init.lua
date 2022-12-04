c0df8fb655b4b0907eafa3c1b41818dd11a11147
Spencer Mehta <spencermehta@gmail.com>
2022-12-04 19:50:48 +0000

    feat(nvim): add more fugitive keybinds

--- nvim/init.lua
+++ nvim/init.lua
@@ -19,3 +19,5 @@ vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 -- fugitive
 vim.keymap.set("n", "<leader>gs", "<CMD>G<CR>", {})
+vim.keymap.set("n", "<leader>gp", "<CMD>G push<CR>", {})
+vim.keymap.set("n", "<leader>gpsu", "<CMD>G push --set-upstream origin HEAD<CR>", {})
